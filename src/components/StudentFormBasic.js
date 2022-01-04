8c7b94ee744eaf3a32719cb1711621e8a9e85246
Sridhar V <sridhar.veesam@gmail.com>
2022-01-04 20:39:30 +0530

    test

---
+++ src/components/StudentFormBasic.js
@@ -0,0 +1,31 @@
+import React from 'react'
+function StudentFormBasic () {
+    return (<div>
+        <form>
+            <label htmlFor='name'>Name</label>
+            <input type="text" id="name" name="name"/>
+            <label htmlFor='email'>Email</label>
+            <input type="text" id="email" name="email"/>
+            <button>Submit</button>
+        </form>
+    </div>);
+}
+
+export default StudentFormBasic
+
+// first name 
+// last name 
+// email 
+// phone number 
+// address
+// house Number
+// Street
+// land mark 
+// permanent address
+// temp address
+// previou qualication 
+// course for enrollmentry {
+    
+// } catch (error) {
+    
+// }
