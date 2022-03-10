b225da1e1cc5ce358e97e88355a92f2a48ab534e
Sridhar V <sridhar.veesam@gmail.com>
2022-03-10 23:28:50 +0530

    changes

--- src/components/StudentFormAdv.js
+++ src/components/StudentFormAdv.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import * as Yup from 'yup'
-import {ErrorMessage, Formik,Form, Field} from 'formik'
+import {ErrorMessage, Formik,Form, Field,FieldArray} from 'formik'
 // below is example for using useFormik hook 
 const initialValues = {
     name:'Enter name',
-    email:''            
+    email:'',
+    address:{line1:'',line2:''},
+    phoneNumbers:['',''],
+    altPhones:['']
 }
 
 const onSubmit = (values) => { 
@@ -39,13 +42,63 @@ function StudentFormAdv () {
             <div className='form-control'>
             <label htmlFor='name'>Name</label>
             <Field type="text" id="name" name="name" />
-            <ErrorMessage name="name"></ErrorMessage>
+            <ErrorMessage name="name" component="div"></ErrorMessage>
             </div>
             <div className='form-control'>
             <label htmlFor='email'>Email</label>
             <Field type="text" id="email" name="email" />
             <ErrorMessage name="email"></ErrorMessage>
             </div>
+            <div className='form-control'>
+            <label htmlFor='address'>Address Line1</label>
+            <Field type="text" id="address.line1" name="address.line1" />
+            <ErrorMessage name="address.line1"></ErrorMessage>
+            </div>
+
+            <div className='form-control'>
+            <label htmlFor='address.line2'>Address Line2</label>
+            <Field type="text" id="address.line2" name="address.line2" />
+            <ErrorMessage name="address.line2"></ErrorMessage>
+            </div>
+
+            <div className='form-control'>
+            <label htmlFor='phoneNumbers[0]'> Phone number 1</label>
+            <Field type="text" id="phoneNumbers[0]" name="phoneNumbers[0]" />
+            <ErrorMessage name="phoneNumbers[0]"></ErrorMessage>
+            </div>
+            <div className='form-control'>
+            <label htmlFor='phoneNumbers[1]'>Phone number 2</label>
+            <Field type="text" id="phoneNumbers[1]" name="phoneNumbers[1]" />
+            <ErrorMessage name="phoneNumbers[1]"></ErrorMessage>
+            </div>
+
+            <div className='form-control'>
+                <FieldArray name='altPhones'>
+                    {
+                    (fieldArrayProps)=>{
+                        console.log('field array props',fieldArrayProps)
+                        const {push,remove,form} = fieldArrayProps
+                        const {values} = form
+                        const {altPhones} = values
+                        return (<div>
+                            {
+                                altPhones.map((phNumber,index)=>(
+                                    <div key={index} >
+                                        <Field name={`altNumbers[${index}]`}/>
+                                        {
+                                            index>0 && <button type="button" onClick={()=>remove(index)}>-</button> 
+                                        }
+                                        
+                                        <button type="button" onClick={()=>push('')}>+</button>
+                                    </div>
+                                ))
+                            }
+                        </div>)
+                    }
+                    }
+                </FieldArray>
+            </div>
+
             <button type="submit">Submit</button>
         </Form>
         </Formik>
